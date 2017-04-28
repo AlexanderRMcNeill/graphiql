df442056d2c0a0164a662335bcf19c175d6a1a8b
Alex McNeill <alexmcneill@Alexs-MacBook-Pro.local>
2017-04-28 12:23:08 +1200

    Allow users to pass in the graphql url in the request

--- client/index.js
+++ client/index.js
@@ -8,12 +8,30 @@ import './style.css';
 
 const data = JSON.parse(document.getElementById('data').innerHTML);
 
+function getUrlParam(name) {
+  if(!window.location.search) return;
+
+  // Getting the query without the ?
+  const parametersString = window.location.search.substring(1);
+
+  let params = {}
+  parametersString.split('&')
+    .map(function(param) {
+      const splitParam = param.split('=');
+      params[splitParam[0]] = splitParam[1];
+    });
+
+  return params[name];
+}
+
 function graphQLFetcher(graphQLParams) {
-  if(!data.url){
-    throw new Error('No url defined: Please set the GRAPHQL_SERVER_URL enviroment variable on the server.')
+  const urlParam = getUrlParam('url');
+  const url = urlParam ? urlParam : data.url;
+  if(!url){
+    throw new Error('No url defined: You can set the default url by setting GRAPHQL_SERVER_URL enviroment variable on the server.')
   }
 
-  return fetch(data.url + '/graphql', {
+  return fetch(url + '/graphql', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(graphQLParams),
